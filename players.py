665083a1b9f1412c05032f2573a32502a7eb7f2b
inkOrCloud <inkorcloud@outlook.com>
2024-09-20 16:27:55 +0800

    Fix the tank to generate in the wall

--- players.py
+++ players.py
@@ -7,15 +7,29 @@ import unit
 
 
 def player0_preset_create(surface:pygame.Surface) -> (unit.Tank, controller.PlayerControl):
-    player = unit.Tank(int(random() * surface.get_width()), int(random() * surface.get_height())
-                       , 0, unit.RED_PATH)
+    player = None
+    is_crash = True
+    while is_crash:
+        player = unit.Tank(int(random() * surface.get_width()), int(random() * surface.get_height())
+                           , 0, unit.RED_PATH)
+        if  pygame.sprite.spritecollide(player, unit.wall_group, False, None):
+            unit.tanks_group.remove(player)
+        else:
+            is_crash = False
     player_control = controller.PlayerControl(
         pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_SPACE, player)
     return player, player_control
 
 def player1_preset_create(surface:pygame.Surface) -> (unit.Tank, controller.PlayerControl):
-    player = unit.Tank(int(random() * surface.get_width()), int(random() * surface.get_height())
-                       , 1, unit.BLUE_PATH)
+    player = None
+    is_crash = True
+    while is_crash:
+        player = unit.Tank(int(random() * surface.get_width()), int(random() * surface.get_height())
+                           , 1, unit.BLUE_PATH)
+        if  pygame.sprite.spritecollide(player, unit.wall_group, False, None):
+            unit.tanks_group.remove(player)
+        else:
+            is_crash = False
     player_control = controller.PlayerControl(
     pygame.K_w, pygame.K_s, pygame.K_d, pygame.K_a, pygame.K_e, player)
     return player, player_control
